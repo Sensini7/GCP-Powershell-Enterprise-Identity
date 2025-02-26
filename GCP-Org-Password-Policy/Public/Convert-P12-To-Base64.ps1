7b690f0762538042da3370893f6e130c84c21e6a
sensini7 <pelekengaih@gmail.com>
2025-02-26 20:27:59 +0100

    chore: developed workflow file

---
+++ GCP-Org-Password-Policy/Public/Convert-P12-To-Base64.ps1
@@ -0,0 +1,9 @@
+# Replace path with your P12 file location
+$p12Path = "C:\Users\Personal\Downloads\mindful-life-450521-e0-c5c74fa10e5c.p12"
+$base64Value = [Convert]::ToBase64String([IO.File]::ReadAllBytes($p12Path))
+
+# Output to a file (optional)
+$base64Value | Out-File "p12_base64.txt"
+
+# Display the value
+Write-Host $base64Value
